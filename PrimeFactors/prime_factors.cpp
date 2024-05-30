fb7f856a32ecbcc621344a8ec1436599a83917ac
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-30 12:15:56 +0900

    [refactor] Cycle 7

--- PrimeFactors/prime_factors.cpp
+++ PrimeFactors/prime_factors.cpp
@@ -5,23 +5,10 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (number > 1) { 
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						ret.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else if (number == 12) {
-				ret.push_back(2);
-				ret.push_back(2);
-				ret.push_back(3);
-			}
-			else {
-				ret.push_back(number);
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				ret.push_back(divisor);
+				number /= divisor;
 			}
 		}
 		return ret;
