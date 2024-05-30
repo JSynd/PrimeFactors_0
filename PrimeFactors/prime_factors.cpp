529260417d91dbfb557c94e111ea36a4f08f77e4
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-30 12:02:15 +0900

    [refactor] Cycle 3

--- PrimeFactors/prime_factors.cpp
+++ PrimeFactors/prime_factors.cpp
@@ -5,8 +5,7 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> ret = {};
-		if (number == 2) { ret.push_back(2); }
-		if (number == 3) { ret.push_back(3); }
+		if (number > 1) { ret.push_back(number); }
 		return ret;
 	};
 };
